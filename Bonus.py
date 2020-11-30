98e274599c78156f4dc4134d4756e09edc09d952
loulou <loulou@DESKTOP-4LTHQCD>
2020-11-30 14:34:45 +0100

    recup

--- Bonus.py
+++ Bonus.py
@@ -9,7 +9,6 @@ class Bonus :
 		self.type = randint(0,typeMax)
 		self.zone_neutre = Zone_neutre
 		self.window = Window
-		
 		self.image=pygame.Surface((GameConfig.bonus_size,GameConfig.bonus_size))
 		pygame.draw.rect(self.image,(0,233,0),(0,0,GameConfig.bonus_size,GameConfig.bonus_size))
 
@@ -19,5 +18,6 @@ class Bonus :
 		
 	def moove(self) :
 		self.X-=self.speed
+
 	def draw(self) : 
 		self.window.blit(self.image,(self.X,self.Y))
